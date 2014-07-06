dfb2596a5780875001f339fedfdaa47723e5a6fd
Thieres Tembra <tdt@mytdt.com.br>
2014-07-05 22:31:15 -0300

    Create MyDatecsSDK.java

---
+++ src/android/MyDatecsSDK.java
@@ -0,0 +1,29 @@
+package br.com.mytdt.print;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+public class MyDatecsSDK extends CordovaPlugin {
+    public static final String ACTION_CONNECT = "connect";
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (ACTION_CONNECT.equals(action) {
+            String message = args.getString(0);
+            this.echo(message, callbackContext);
+            return true;
+        }
+        return false;
+    }
+
+    private void echo(String message, CallbackContext callbackContext) {
+        if (message != null && message.length() > 0) {
+            callbackContext.success(message);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+}
