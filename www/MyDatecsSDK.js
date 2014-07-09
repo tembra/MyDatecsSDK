8e350a981966f37f0ad62bee8818dee3faddfa70
Thieres Tembra <tdt@mytdt.com.br>
2014-07-08 21:15:46 -0300

    Plugin test 19

--- www/MyDatecsSDK.js
+++ www/MyDatecsSDK.js
@@ -21,9 +21,45 @@ var myDatecsSDK = {
         exec(onSuccess, onError, 'MyDatecsSDK', 'disconnect', []);
     },
 
+    reset: function(onSuccess, onError) {
+        exec(onSuccess, onError, 'MyDatecsSDK', 'reset', []);
+    },
+
     printText: function(text, charset, onSuccess, onError) {
         exec(onSuccess, onError, 'MyDatecsSDK', 'printText', [text, charset]);
+    },
+
+    printTaggedText: function(text, charset, onSuccess, onError) {
+        exec(onSuccess, onError, 'MyDatecsSDK', 'printTaggedText', [text, charset]);
+    },
+
+    feedPaper: function(lines, onSuccess, onError) {
+        exec(onSuccess, onError, 'MyDatecsSDK', 'feedPaper', [lines]);
+    },
+
+    flush: function(onSuccess, onError) {
+        exec(onSuccess, onError, 'MyDatecsSDK', 'flush', []);
+    },
+
+    selectPageMode: function(onSuccess, onError) {
+        exec(onSuccess, onError, 'MyDatecsSDK', 'selectPageMode', []);
+    },
+
+    selectStandardMode: function(onSuccess, onError) {
+        exec(onSuccess, onError, 'MyDatecsSDK', 'selectStandardMode', []);
+    },
+
+    printPage: function(onSuccess, onError) {
+        exec(onSuccess, onError, 'MyDatecsSDK', 'printPage', []);
+    },
+
+    setAlign: function(align, onSuccess, onError) {
+        exec(onSuccess, onError, 'MyDatecsSDK', 'setAlign', [align]);
+    },
+
+    printSelfTest: function(onSuccess, onError) {
+        exec(onSuccess, onError, 'MyDatecsSDK', 'printSelfTest', []);
     }
 };
 
-module.exports  = myDatecsSDK;
+module.exports = myDatecsSDK;
